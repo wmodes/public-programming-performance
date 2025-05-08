2409629bcfbf27e7c44661008210893693d1977c
terumafu <119095669+terumafu@users.noreply.github.com>
2025-05-08 16:52:51 -0700

    Updated WanderNPC

--- 2025spring/js/WanderNPC.js
+++ 2025spring/js/WanderNPC.js
@@ -1,12 +1,11 @@
 class WanderNPC extends NPC{
-    constructor(x,y,speed){
-        super(x,y,speed);
-
+    update(){
+        this.move([
+            Math.pow(-1,get_random_int_between_inclusive(0,1)),
+            Math.pow(-1,get_random_int_between_inclusive(0,1))])
     }
-    draw(){
-
-    }
-    move(){
-        
-    }
+}
+function get_random_int_between_inclusive(x,y){
+    return Math.floor(Math.random() * (y + 1 - x)) + x
+    
 }
