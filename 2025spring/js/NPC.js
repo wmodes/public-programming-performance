1581a9873b0506275cb4f5f73621eb501cccbcb9
terumafu <119095669+terumafu@users.noreply.github.com>
2025-05-08 16:54:43 -0700

    moved wander action up to NPC

--- 2025spring/js/NPC.js
+++ 2025spring/js/NPC.js
@@ -13,7 +13,16 @@ class NPC {
         this.y += arr[1];
     }
     update(){
-        
+
+    }
+    wander(){
+        this.move([
+            Math.pow(-1,get_random_int_between_inclusive(0,1)),
+            Math.pow(-1,get_random_int_between_inclusive(0,1))])
     }
 }
 
+function get_random_int_between_inclusive(x,y){
+    return Math.floor(Math.random() * (y + 1 - x)) + x
+    
+}
