d4d9a8ede92e2616ee7f6b5fe4082a14ee3655ae
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-05-13 16:36:31 -0700

    created boat class

---
+++ 2025spring/js/boat.js
@@ -0,0 +1,19 @@
+class Boat extends PathfindingNPC {
+    constructor(x, y, speed, boatSprite) {
+        super(x, y, speed, objective, path); // not sure about those last 2
+        this.sprite = boatSprite;
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
