a777b5125c42985c59947fd5404a32b0adcf43b1
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-05-13 16:57:23 -0700

    updated boat draw

--- 2025spring/js/classes/npc/boat.js
+++ 2025spring/js/classes/npc/boat.js
@@ -1,24 +1,21 @@
 class Boat extends PathfindingNPC {
-    constructor(x, y, speed, imagePath) {
+    constructor(x, y, speed/*, imagePath*/) {
         super(x, y, speed, objective, path); // not sure about those last 2
-        this.image = loadImage(imagePath);
+        // this.image = loadImage(imagePath);
+        
     }
 
     draw() {
-        push();
-        imageMode(CENTER);
-        image(this.image, this.x, this.y);
-        pop();
+        // push();
+        // imageMode(CENTER);
+        // image(this.image, this.x, this.y);
+        // pop();
+        stroke(0, 255, 0, 128);
+        beginShape();
+        vertex(-this.tw, 0);
+        vertex(0, this.th);
+        vertex(this.tw, 0);
+        vertex(0, -this.th);
+        endShape();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
