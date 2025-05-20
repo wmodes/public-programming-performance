637aa7639a68ce53982a8979ccd142b6026fbd06
terumafu <119095669+terumafu@users.noreply.github.com>
2025-05-20 16:18:45 -0700

    added animation support

--- 2025spring/js/classes/npc/npc.js
+++ 2025spring/js/classes/npc/npc.js
@@ -36,9 +36,10 @@ class NPC {
   
     // Move randomly in a cardinal direction
     wander() {
-        let arr = [[0,1],[0,-1],[1,0],[-1,0]]
-        
-      this.move(arr[get_random_int_between_inclusive(0,3)]);
+      let arr = [[0,1],[0,-1],[1,0],[-1,0]]
+      let temp = get_random_int_between_inclusive(0,3)
+      this.move(arr[temp]);
+      return temp;
     }
 }
   
